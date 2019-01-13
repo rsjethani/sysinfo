719243a29e03722c12845b755cdf318e373c893c
Ravi Shekhar Jethani <rsjethani@gmail.com>
2019-01-13 18:35:22 +0530

    Convert numeric values to uint64

--- builtins/battery.go
+++ builtins/battery.go
@@ -4,6 +4,8 @@ import (
 	csv "encoding/csv"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/rsjethani/sysinfo/interfaces"
@@ -42,23 +44,36 @@ func (b *battery) Type() string {
 
 func BatteryInit() (interfaces.InfoProvider, error) {
 	b := battery{devType: "battery", category: "hardware"}
+	files, _ := filepath.Glob("/sys/class/power_supply/BAT*/uevent")
+	if len(files) == 0 {
+		return nil, fmt.Errorf("Error locating battery info. Either battery not present or not enough permissions")
+	}
+	for _, file := range files {
+		f, err := os.Open(file)
+		defer f.Close()
+		if err != nil {
+			return nil, err
+		}
 
-	f, err := os.Open("/sys/class/power_supply/BAT0/uevent")
-	defer f.Close()
-	if err != nil {
-		return nil, err
-	}
+		r := csv.NewReader(f)
+		r.Comma = 61 //ASCII value of "="
+		records, err := r.ReadAll()
+		if err != nil {
+			return nil, err
+		}
 
-	r := csv.NewReader(f)
-	r.Comma = 61 //ASCII value of "="
-	records, err := r.ReadAll()
-	if err != nil {
-		return nil, err
+		m := make(map[string]interface{})
+		for _, rec := range records {
+			key := strings.Split(rec[0], "POWER_SUPPLY_")[1]
+			// convert values like capacity, charge etc to uint64
+			m[key], err = strconv.ParseUint(rec[1], 10, 64)
+			// Error means the value is alphanumeric value like model no., mannufacturer etc., keep these as strings
+			if err != nil {
+				m[key] = rec[1]
+			}
+		}
+
+		b.atttributes = append(b.atttributes, &m)
 	}
-	m := make(map[string]interface{})
-	for _, rec := range records {
-		m[strings.Split(rec[0], "POWER_SUPPLY_")[1]] = rec[1]
-	}
-	b.atttributes = append(b.atttributes, &m)
 	return &b, nil
 }
